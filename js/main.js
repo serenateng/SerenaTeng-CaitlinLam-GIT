04791c0be417211b1189cfcdfc4be303c253f075
serenateng <steng8888@gmail.com>
2016-04-02 21:13:39 -0400

    adding template html files

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+
+//self executing anonymous function - use this. prevents global variables
+
+(function() {
+	console.log("yup the SEAF fired");
+	//variables
+	//functions
+	//listeners
+})();
